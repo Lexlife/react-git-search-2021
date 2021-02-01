c7f4d8546e86283e3bf7ee767d8d9cdb8640abb6
Alexander <admin@lleexx.ru>
2021-02-02 01:54:45 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className="card">
+      <div className="card-body" style={{alignItems: 'center'}}>
+        Версия приложения: <strong>1.0.0</strong>
+      </div>
+    </div>
+  )
+}
